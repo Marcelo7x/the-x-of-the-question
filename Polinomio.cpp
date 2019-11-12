d46ebf66de3c8b2907b33eafd0008309b8eb12a1
taty2314 <54770876+taty2314@users.noreply.github.com>
2019-11-12 13:44:14 -0300

    Add files via upload

---
+++ Polinomio.cpp
@@ -0,0 +1,31 @@
+#include "Polinomio.h"
+#include <iostream>
+using namespace std;
+
+//Construtor padrão. Representa numero 0
+Polinomio::Polinomio() {
+
+}
+
+//Cria um polinomio a partir de um numero inteiro (positivo ou negativo)
+Polinomio::Polinomio(const int) {
+
+}
+
+//Cria um polinomio a partir de um vetor de coeficientes. Os coeficientes deverão ser o do polinomio (Posicao 0 sera a_0, posicao 1 sera a_1, ...). O primeiro parametro representa o grau do polinomio.
+Polinomio::Polinomio(unsigned int n, double *a) {
+
+}
+
+//Construtor de cópia
+Polinomio::Polinomio(const Polinomio &poli) {
+
+}
+
+//Destrutor
+Polinomio::~Polinomio() {
+
+}
+
+//OPERADORES
+
